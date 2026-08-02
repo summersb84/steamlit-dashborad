7a01108c2534bd46457ac00ef1e461425d69132a
sb <summersb84@gmail.com>
2026-08-02 18:32:43 +0900

    update dashboard title

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.title("나의 첫 Streamlit 앱")
 
@@ -13,3 +14,15 @@ data = {
 df = pd.DataFrame(data)
 
 st.dataframe(df)
+
+
+st.title("고객 분석 Dashboard")
+
+df = pd.DataFrame({
+    "고객": ["A", "B", "C"],
+    "매출": [100, 200, 300]
+})
+
+fig = px.bar(df, x="고객", y="매출")
+
+st.plotly_chart(fig)
