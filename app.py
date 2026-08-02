64eb4475421677c6b1f312db89a373a46489c639
sb <summersb84@gmail.com>
2026-08-02 19:26:23 +0900

    database upload

--- app.py
+++ app.py
@@ -26,7 +26,16 @@ def get_connection():
 
 conn = get_connection()
 
+# DB 연결 확인
+st.write("DB 연결 성공")
 
+tables = pd.read_sql("""
+SELECT name
+FROM sqlite_master
+WHERE type='table'
+""", conn)
+
+st.write(tables)
 
 # ------------------
 # 데이터 함수
